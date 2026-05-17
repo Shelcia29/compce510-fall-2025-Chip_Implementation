7d6e914d40f3f80875f4ad74be0ece7ff0269163
Shelcia29 <60932458+Shelcia29@users.noreply.github.com>
2026-05-17 22:38:48 +0300

    Add files via upload

---
+++ 16/ex8/flow_datapath_top_1/dbs/postroute.enc/extraction/edi_pe_globals.tcl
@@ -0,0 +1,5 @@
+setUserDataValue edi_pe::pegAssignedWHLRatio {0.0104}
+setUserDataValue edi_pe::pegPreRouteXcapTimeCriticalityScale {0.0017}
+setUserDataValue edi_pe::pegUnassignedWHLRatio {0.5895}
+setUserDataValue edi_pe::pegWeightMultiplier2ForHLS {1.9519}
+setUserDataValue edi_pe::pegWireCapacityRatio {0.7727}
