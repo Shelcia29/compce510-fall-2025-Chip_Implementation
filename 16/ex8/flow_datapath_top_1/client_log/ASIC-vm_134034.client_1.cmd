47ee32f07bdef6dd9af4e764129d8a8132c567d6
Shelcia29 <60932458+Shelcia29@users.noreply.github.com>
2026-05-17 20:58:30 +0300

    Add files via upload

---
+++ 16/ex8/flow_datapath_top_1/client_log/ASIC-vm_134034.client_1.cmd
@@ -0,0 +1,18 @@
+#######################################################
+#                                                     
+#  Innovus Command Logging File                     
+#  Created on Fri Nov 14 16:57:43 2025                
+#                                                     
+#######################################################
+
+#@(#)CDS: Innovus v23.15-s106_1 (64bit) 07/16/2025 17:15 (Linux 3.10.0-693.el7.x86_64)
+#@(#)CDS: NanoRoute 23.15-s106_1 NR250707-2219/23_15-UB (database version 18.20.674) {superthreading v2.20}
+#@(#)CDS: AAE 23.15-s032 (64bit) 07/16/2025 (Linux 3.10.0-693.el7.x86_64)
+#@(#)CDS: CTE 23.15-s037_1 () Jul 16 2025 02:46:10 ( )
+#@(#)CDS: SYNTECH 23.15-s011_1 () Jun 23 2025 00:02:58 ( )
+#@(#)CDS: CPE v23.15-s089
+#@(#)CDS: IQuantus/TQuantus 23.1.1-s476 (64bit) Wed Jul 2 23:22:08 PDT 2025 (Linux 3.10.0-693.el7.x86_64)
+
+set_global _enable_mmmc_by_default_flow      $CTE::mmmc_default
+suppressMessage ENCEXT-2799
+getVersion
