9859c4b089be0472c2e827ead21e8ac9edde30e8
Shelcia29 <60932458+Shelcia29@users.noreply.github.com>
2026-05-17 20:06:31 +0300

    Add files via upload

---
+++ 16/ex8/flow_matmul_sync_1/dbs/floorplan.enc/extraction/edi_pe_globals.tcl
@@ -0,0 +1,5 @@
+setUserDataValue edi_pe::pegPreRouteXcapTimeCriticalityScale {0.0016}
+setUserDataValue edi_pe::pegUnassignedWHLRatio {0.237}
+setUserDataValue edi_pe::pegWeightMultiplier2ForHLS {1.9318}
+setUserDataValue edi_pe::pegWireCapacityRatio {0.7727}
+setUserDataValue edi_pe::pegAssignedWHLRatio {0}
