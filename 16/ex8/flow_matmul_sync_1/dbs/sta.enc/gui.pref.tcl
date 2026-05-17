f8574118a9bcde46c4be2dcbb18013bfdea4c62b
Shelcia29 <60932458+Shelcia29@users.noreply.github.com>
2026-05-17 20:26:59 +0300

    Add files via upload

---
+++ 16/ex8/flow_matmul_sync_1/dbs/sta.enc/gui.pref.tcl
@@ -0,0 +1,53 @@
+################################################
+#                                              #
+#  Tempus User Preference File         #
+#                                              #
+################################################
+#
+# Created by student
+# Created on Fri Nov 14 15:49:41 EET 2025
+# Version 1.1
+#
+
+################################################
+#  Syntax for Console-related pref:            #
+#  setPreference <preference_name> <value>     #
+################################################
+setPreference ShowStatusBar 1
+setPreference ShowMultMatch 1
+setPreference MaxLineLimit 100000
+setPreference WebBrowser mozilla
+setPreference lineWrapMode char
+setPreference historyWSide bottom
+setPreference MainConsoleColor white
+setPreference PtiColor white
+setPreference CelticColor white
+################################################
+#  Syntax for Design-related/non-color pref:   #
+#  setPreference <preference_name> <value>     #
+################################################
+setPreference DesignHierChar {/}
+setPreference DEFHierChar {/}
+setPreference PDEFBusDelim {[]}
+setPreference LecDofile 0
+setPreference CmdLogMode 1
+setPreference LogTypeInCmd 0
+
+################################################
+#  Syntax for Display-related/non-color pref:  #
+#  setPreference <preference_name> <value>     #
+################################################
+setPreference InstanceText Instance
+setPreference zoomPrevCount 9
+setPreference AutoRedraw 1
+setPreference ShowUnplacedInst 0
+setPreference scaleOfZoom 3.0
+setPreference DisplayPtnPin 1
+setPreference percentageOfPan 30
+setPreference ShowLayerDirection 0
+setPreference SelectByArea 0
+setPreference SelectNetWhenSelectPin 0
+setPreference HiliteNetWhenQueryObj 0
+setPreference QueryWireNet 0
+setPreference WinSelectMargin 8
+
---
+++ 16/ex8/flow_matmul_sync_1/dbs/sta.enc/mmmc/views/slow_0p9v_125c_cmax_func/latency.sdc
@@ -0,0 +1,4 @@
+set_clock_latency -source -early -min -fall -0.1 [get_clocks {clk_i}]
+set_clock_latency -source -early -max -fall -0.1 [get_clocks {clk_i}]
+set_clock_latency -source -late -min -fall 0.1 [get_clocks {clk_i}]
+set_clock_latency -source -late -max -fall 0.1 [get_clocks {clk_i}]
