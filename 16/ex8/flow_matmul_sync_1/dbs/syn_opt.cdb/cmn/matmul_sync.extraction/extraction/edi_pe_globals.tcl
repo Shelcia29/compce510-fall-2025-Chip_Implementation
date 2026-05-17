1ca8edc6b81b799f374c6595cf9c8c453f30fe5e
Shelcia29 <60932458+Shelcia29@users.noreply.github.com>
2026-05-17 20:28:35 +0300

    Add files via upload

---
+++ 16/ex8/flow_matmul_sync_1/dbs/syn_opt.cdb/cmn/matmul_sync.extraction/extraction/edi_pe_globals.tcl
@@ -0,0 +1,5 @@
+setUserDataValue edi_pe::pegPreRouteXcapTimeCriticalityScale {0.0016}
+setUserDataValue edi_pe::pegUnassignedWHLRatio {0.237}
+setUserDataValue edi_pe::pegWeightMultiplier2ForHLS {1.6668}
+setUserDataValue edi_pe::pegWireCapacityRatio {0.6667}
+setUserDataValue edi_pe::pegAssignedWHLRatio {0}
